09f5cbc6612f134aa7d3aa057b8e0af89000024a
Oscar Echeverri <oscar@echeverri.ca>
2025-12-02 16:59:25 -0500

    Day02 - Part02 Solution

--- Sources/Solutions/Day02.swift
+++ Sources/Solutions/Day02.swift
@@ -11,7 +11,7 @@ public struct Day02: Solution {
     public var day: Int { 2 }
     public var year: Int { 2025 }
 
-    let numbers: [Int]
+    let numbers: [[Int]]
 
     public init(input: String) {
         numbers =
@@ -20,41 +20,107 @@ public struct Day02: Solution {
             .map {
                 $0.components(separatedBy: "-")
             }
-            .flatMap {
-                ClosedRange(uncheckedBounds: (Int($0[0])!, Int($0[1])!))
-                    .map(\.self)
+            .compactMap { parts -> [Int]? in
+                guard parts.count == 2,
+                    let lowerBound = Int(parts[0]),
+                    let upperBound = Int(parts[1])
+                else { return nil }
+                return Array(lowerBound ... upperBound)
             }
+            .flatMap(\.self)
+            .map(\.digitsOf)
 
     }
 
-    private func getDivisor(_ n: Int) -> Int {
-        let value = round(log10(Double(n)) / log10(100.0))
+    func isValid(_ n: [Int]) -> Bool {
 
-        return Int(pow(10, value))
+        guard n.count % 2 == 0 else { return true }
+
+        var leftPtr = 0
+        var rightPtr = n.count / 2
+
+        repeat {
+            if n[leftPtr] != n[rightPtr] {
+                return true
+            }
+            leftPtr += 1
+            rightPtr += 1
+        } while rightPtr < n.endIndex
+
+        return false
+
+    }
+    func isinValidWithSlidingWindow(_ n: [Int]) -> Bool {
+        !isValidWithSlidingWindow(n)
     }
 
-    func isValid(_ n: Int) -> Bool {
+    func isValidWithSlidingWindow(_ n: [Int]) -> Bool {
+        guard n.count >= 2 else { return true }
 
-        let divisor = getDivisor(n)
+        var windowLength = 1
+        let midpoint = n.count / 2
+        while windowLength <= midpoint {
+            if n.count % windowLength != 0 {
+                windowLength += 1
+                continue
+            }
+            var right = windowLength
+            let currentSlice = n[0 ..< windowLength]
+            var nextSlice = n[right ..< right + windowLength]
 
-        let leftHalf = Int(n / divisor)
-        let rightHalf = n - (leftHalf * divisor)
-
-        return leftHalf != rightHalf
+            while nextSlice == currentSlice {
+                right += windowLength
+                if right == n.count {
+                    return false
+                }
+                nextSlice = n[right ..< right + windowLength]
+            }
+            windowLength += 1
+        }
+        return true
 
     }
 
-    func isInvalid(_ n: Int) -> Bool {
+    func isInvalid(_ n: [Int]) -> Bool {
         !isValid(n)
     }
 
     public func solvePart1() -> Int {
         numbers
             .filter(isInvalid(_:))
+            .map(\.asSingleInt)
             .reduce(0, +)
+
     }
 
     public func solvePart2() -> Int {
-        return 0
+        numbers
+            .filter(isinValidWithSlidingWindow(_:))
+            .map(\.asSingleInt)
+            .reduce(0, +)
+    }
+}
+
+extension Int {
+    var digitsOf: [Int] {
+        var num = abs(self)
+        var digits: [Int] = []
+
+        if num == 0 {
+            return [0]
+        }
+
+        while num > 0 {
+            digits.insert(num % 10, at: 0)
+            num /= 10
+        }
+
+        return digits
+    }
+}
+
+extension Array where Element == Int {
+    var asSingleInt: Int {
+        reduce(0) { $0 * 10 + $1 }
     }
 }
--- Tests/SolutionsTests/Day02Tests.swift
+++ Tests/SolutionsTests/Day02Tests.swift
@@ -1,3 +1,4 @@
+import InputFetcher
 //
 //  Day02Tests.swift
 //  AoC2025
@@ -13,15 +14,72 @@ struct Day02Tests {
     @Test(
         "Tests whether or not our algorithm for checking validity works",
         arguments: zip(
-            [99, 101, 22, 251251, 9999, 999999, 999998, 100100, 1010, 5050, 5051],
-            [true, false, true, true, true, true, false, true, true, true, false]
+            [99, 101, 22, 251251, 9999, 999999, 999998, 100100, 1010, 5050, 5051, 999],
+            [false, true, false, false, false, false, true, false, false, false, true, true]
         )
     )
 
     func checksValidityValidly(_ input: Int, _ expected: Bool) {
         let sol = Day02(input: "67-69")
+        #expect(sol.isValid(input.digitsOf) == expected)
 
-        #expect(sol.isValid(input) == expected)
+    }
 
+    @Test(
+        "Tests whether sliding window parirings works",
+        arguments: zip(
+            [
+                [1, 2, 1, 2, 1],
+                [1],
+                [1, 2, 1, 2],
+                [1, 1],
+                [1, 1, 1],
+                [1, 2, 3, 4, 1, 2, 3, 4],
+            ],
+            [
+                true,
+                true,
+                false,
+                false,
+                false,
+                false,
+            ]
+        )
+    )
+    func checksValidityWithSlidingWindow(_ input: [Int], _ expected: Bool) {
+        let sol = Day02(input: "67-69")
+        #expect(sol.isValidWithSlidingWindow(input) == expected)
+    }
+
+    @Test func `Test Part 1 Solution`() async throws {
+        let sampleFetcher = CopyPastaFetcher(sampleData: true)
+        let realFetcher = CopyPastaFetcher(sampleData: false)
+
+        let solSample = try await Day02(
+            input: sampleFetcher.inputFor(day: 2, year: 2025)
+        )
+
+        let solReal = try await Day02(
+            input: realFetcher.inputFor(day: 2, year: 2025)
+        )
+
+        #expect(solSample.solvePart1() == 1_227_775_554)
+        #expect(solReal.solvePart1() == 31_000_881_061)
+    }
+
+    @Test func `Test Part 2 Solution`() async throws {
+        let sampleFetcher = CopyPastaFetcher(sampleData: true)
+        let realFetcher = CopyPastaFetcher(sampleData: false)
+
+        let solSample = try await Day02(
+            input: sampleFetcher.inputFor(day: 2, year: 2025)
+        )
+
+        let solReal = try await Day02(
+            input: realFetcher.inputFor(day: 2, year: 2025)
+        )
+
+        #expect(solSample.solvePart2() == 4_174_379_265)
+        #expect(solReal.solvePart2() == 46_769_308_485)
     }
 }
