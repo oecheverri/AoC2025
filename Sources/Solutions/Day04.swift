dc1b03b90174df0a3657cba3a32d09c09223426b
Oscar Echeverri <oscar@echeverri.ca>
2025-12-05 23:41:22 -0500

    Day04 Part 1

---
+++ Sources/Solutions/Day04.swift
@@ -0,0 +1,25 @@
+//
+//  Day04.swift
+//  AoC2025
+//
+//  Created by Oscar Echeverri on 2025-12-04.
+//
+
+public struct Day04: Solution {
+
+    public let day = 5
+    public let year = 2025
+
+    public init(input: String) {
+
+    }
+
+    public func solvePart1() -> Int {
+        0
+    }
+
+    public func solvePart2() -> Int {
+        0
+    }
+
+}
